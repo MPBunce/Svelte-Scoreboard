5e0ec3474a8574a16750287a71f44504f5b25d28
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-23 10:59:39 -0400

    done basics

---
+++ svelte-scoreboard/src/lib/Player.svelte
@@ -0,0 +1,63 @@
+<script>
+
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+  
+    export let name ;
+    export let points;
+    export let showControls = false;
+  
+    const addPoint = () => (points += 1);
+    const removePoint = () => (points -= 1);
+    const toggleControl = () => ( showControls = !showControls)
+  
+    const onDelete = () => {
+
+        dispatch("deleteplayer", name)
+
+    };
+     
+  </script>
+  
+<main>
+  
+
+  
+
+    <div class="card">
+
+        <h1>
+            {name}
+            <button class="btn btn-sm" on:click={toggleControl}>
+  
+            {#if showControls}-{:else}+{/if}
+            </button>
+
+            <button class="btn btn-sm btn-danger" on:click={onDelete}>X</button>      
+        </h1>
+        <h3>Points: {points}</h3>   
+
+        {#if showControls}
+            <button class="btn" on:click={addPoint}>+1</button>
+            <button class="btn btn-dark" on:click={removePoint}>-1</button>
+            <input type="number" bind:value={points}>
+
+        {/if}
+
+
+    </div>
+
+  
+  
+    
+</main>
+  
+<style>
+    h1 {
+        color: steelblue;
+    }
+    h3{
+        margin-bottom: 10px;
+    }
+</style>
+  
